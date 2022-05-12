44e028e445ed4eb61d529a9df862d8a12a0066a1
Juan Manuel <juanmanuel273816381@gmail.com>
2022-05-12 15:36:16 -0500

    First Deployment

---
+++ src/GifExpertApp.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import AddCategory from './components/AddCategory'
+import GifGrid from './components/GifGrid'
+
+const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch'])
+
+    // const handleAdd = () => {
+    //     setCategories([...categories, 'Stranger Things']);
+    //     // setCategories( cats => [ 'Stranger Things', ...cats ]);
+    // }
+
+    return (
+        <>
+            <h2> Gif Expert App </h2>            
+            <AddCategory setCategories={ setCategories }/>
+            <hr/>
+
+
+            {
+                categories.map( (category) =>
+                    <GifGrid 
+                        key={ category }
+                        category={ category }/>
+                )
+            }
+        </>
+    )
+}
+
+export default GifExpertApp
